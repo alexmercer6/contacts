8290858ce3a924492b52680569f0e0919c2ceba9
alexmercer <alex.mercer6@hotmail.com>
2022-09-09 22:41:24 +1000

    added a useable navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Contacts from "./components/Contacts"
 import Home from "./components/Home"
 import Contact from "./components/Contact"
+import Navbar from "./components/Navbar"
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route exact path="/contacts" element={<Contacts />} />
-                    <Route exact path="/contact/:id" element={<Contact />} />
-                </Routes>
+                <Navbar />
+                <main className="content-container">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route exact path="/contacts" element={<Contacts />} />
+                        <Route
+                            exact
+                            path="/contact/:id"
+                            element={<Contact />}
+                        />
+                    </Routes>
+                </main>
             </BrowserRouter>
         </div>
     )
