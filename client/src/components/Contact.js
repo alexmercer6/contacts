637d256badf47e832463700d37844e28e78abf46
alexmercer <alex.mercer6@hotmail.com>
2022-09-09 21:45:12 +1000

    added react icons package

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,19 +1,27 @@
 import React from "react"
+import { FcTwoSmartphones, FcHome, FcGlobe, FcInvite } from "react-icons/fc"
 
-function Contact({
-    id,
-    name,
-    username,
-    email,
-    address,
-    phone,
-    website,
-    company,
-}) {
+function Contact({ name, username, email, address, phone, website, company }) {
     return (
         <div>
             <h1>{name}</h1>
-            <p>{email}</p>
+            <p>
+                <FcInvite />
+                {email}
+            </p>
+            <p>
+                <FcTwoSmartphones />
+                {phone}
+            </p>
+            <p>
+                <FcHome />
+                {address.street}, {address.suite}, {address.city},{" "}
+                {address.zipcode}
+            </p>
+            <p>
+                <FcGlobe />
+                {website}
+            </p>
         </div>
     )
 }
