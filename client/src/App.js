d1b99ae3966ba58894afa60be2d4951168bcf57f
alexmercer <alex.mercer6@hotmail.com>
2022-09-09 20:49:15 +1000

    retrieved users with axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,20 @@
 import "./App.css"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 function App() {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const response = await axios.get(
+                "https://jsonplaceholder.typicode.com/users"
+            )
+            setUsers(response.data)
+        }
+        getUsers()
+    }, [])
+
     return <div className="App"></div>
 }
 
